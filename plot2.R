f02d6562707433ce66cd902c7d7f162536f53525
asialab <alicjakundzicz@gmail.com>
2015-08-10 00:42:04 +0200

    plot2
    correctly done

--- plot2.R
+++ plot2.R
@@ -3,11 +3,15 @@ data[,'Date'] <- as.Date(data[,'Date'],"%d/%m/%Y")
 useddates <- as.Date(c("01/02/2007","02/02/2007"),"%d/%m/%Y")
 used <- data[data$Date %in% useddates,]   ## choose needed dates
 
+x <- paste(used$Date,used$Time)
+timeline <- strptime(x,"%Y-%m-%d %H:%M:%S")
+head(timeline)
+
 library(datasets)
 used$Global_active_power[used$Global_active_power=="?"] <- NA
 used$Global_active_power <- as.numeric(levels(used$Global_active_power)[used$Global_active_power])
 
-with(used, plot(Date,Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type="l"))
+plot(timeline,used$Global_active_power, ylab="Global Active Power (kilowatts)", xlab="", type="l")
      
 dev.copy(png, file = "plot2.png")
 dev.off()
